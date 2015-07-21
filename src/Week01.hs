142f0d35e3b54fce21e5427bf27f174c5f1ca27d
Do Nhat Minh <mrordinaire@gmail.com>
2015-07-21 11:13:19 +0800

    changed style and added one more base case for hanoi

--- src/Week01.hs
+++ src/Week01.hs
@@ -31,7 +31,9 @@ type Peg = String
 type Move = (Peg, Peg)
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi 0 _ _ _ = []
-hanoi n src dst tmp = hanoi (n - 1) src tmp dst ++
-                      [(src, dst)] ++
-                      hanoi (n - 1) tmp dst src
+hanoi n src dst tmp
+  | n == 0    = []
+  | n == 1    = [(src, dst)]
+  | otherwise = hanoi (n - 1) src tmp dst ++
+                hanoi 1 src dst tmp ++
+                hanoi (n - 1) tmp dst src
