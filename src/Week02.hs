8935808ed1ba0a00cefe12050f93780142f9602a
Do Nhat Minh <mrordinaire@gmail.com>
2015-08-01 23:26:35 +0800

    finished Week 02 (without optional exercise)

--- src/Week02.hs
+++ src/Week02.hs
@@ -1,5 +1,7 @@
 module Week02 where
 
+import Data.Function (on)
+
 -- Mastermind -----------------------------------------
 
 -- A peg can be one of six colors
@@ -18,6 +20,57 @@ data Move = Move Code Int Int
 colors :: [Peg]
 colors = [Red, Green, Blue, Yellow, Orange, Purple]
 
+-- Exercise 1 -----------------------------------------
+
 -- Get the number of exact matches between the actual code and the guess
 exactMatches :: Code -> Code -> Int
 exactMatches a b = length . filter (uncurry (==)) $ zip a b
+
+-- Exercise 2 -----------------------------------------
+
+-- For each peg in xs, count how many times is occurs in ys
+countColors :: Code -> [Int]
+countColors ps = map (\x -> length $ filter (== x) ps) colors
+
+-- Count number of matches between the actual code and the guess
+matches :: Code -> Code -> Int
+matches xs ys = sum . map (uncurry min) $ (zip `on` countColors) xs ys
+
+-- Exercise 3 -----------------------------------------
+
+-- Construct a Move from a guess given the actual code
+getMove :: Code -> Code -> Move
+getMove secret guess = Move guess em (matches secret guess - em)
+  where em = exactMatches secret guess
+
+-- Exercise 4 -----------------------------------------
+
+isConsistent :: Move -> Code -> Bool
+isConsistent (Move m em nem) guess = em == em' && nem == nem'
+  where Move _ em' nem' = getMove m guess
+
+-- Exercise 5 -----------------------------------------
+
+filterCodes :: Move -> [Code] -> [Code]
+filterCodes move = filter (isConsistent move)
+
+-- Exercise 6 -----------------------------------------
+
+allCodes :: Int -> [Code]
+allCodes n
+  | n == 0    = []
+  | n == 1    = map (: []) colors
+  | otherwise = [peg : code | code <- allCodes (n - 1), peg <- colors]
+
+-- Exercise 7 -----------------------------------------
+
+solve :: Code -> [Move]
+solve secret = go (allCodes $ length secret) []
+  where go [] acc = reverse acc
+        go (g:guesses) acc = let move = getMove secret g
+                             in go (filterCodes move guesses)  (move:acc)
+
+-- Bonus ----------------------------------------------
+
+fiveGuess :: Code -> [Move]
+fiveGuess = undefined
--- tests/Week02Spec.hs
+++ tests/Week02Spec.hs
@@ -9,3 +9,25 @@ spec = do
     it "should return the number of exact matches between the actual code and the guess" $ do
       exactMatches [Red, Blue, Green, Yellow] [Blue, Green, Yellow, Red]   `shouldBe` 0
       exactMatches [Red, Blue, Green, Yellow] [Red, Purple, Green, Orange] `shouldBe` 2
+
+  describe "countColors" $ do
+    it "should count the number of times a peg appears in a list" $ do
+      countColors [Red, Blue, Yellow, Purple] `shouldBe` [1, 0, 1, 1, 0, 1]
+      countColors [Green, Blue, Green, Orange] `shouldBe` [0, 2, 1, 0, 1, 0]
+    it "should return a list of 6 items regardless of the length of the input code" $
+      length (countColors [Red]) `shouldBe` 6
+    it "should return a list whose sum is equal to the length of the input code" $
+      sum (countColors [Red, Blue, Yellow, Purple]) `shouldBe` length [Red, Blue, Yellow, Purple]
+
+  describe "matches" $
+    it "should count the number of matches between the actual code and the guess" $
+      matches [Red, Blue, Yellow, Orange] [Red, Orange, Orange, Blue] `shouldBe` 3
+
+  describe "getMove" $
+    it "should construct a move from a guess given the actual code" $
+      getMove [Red, Blue, Yellow, Orange] [Red, Orange, Orange, Blue] `shouldBe` Move [Red, Orange, Orange, Blue] 1 2
+
+  describe "isConsistent" $
+    it "should work" $ do
+      isConsistent (Move [Red, Red, Blue, Green] 1 1) [Red, Blue, Yellow, Purple] `shouldBe` True
+      isConsistent (Move [Red, Red, Blue, Green] 1 1) [Red, Blue, Red, Purple] `shouldBe` False
